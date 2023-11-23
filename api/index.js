3a2f2c475f2da8b2ed423e9881010cb07fb913f9
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-11-23 21:43:10 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to Mongodb"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
