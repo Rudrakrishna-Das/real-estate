2b3a0e66dfe7a3658d39234c1d12746151380dd8
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-12-07 22:37:38 +0530

    image upload funtionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,16 +1,80 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [formData, setFormData] = useState({});
+  const [fileUploaded, setFileUploaded] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(null);
+
+  useEffect(() => {
+    if (file) {
+      fileHandle(file);
+    }
+  }, [file]);
+  const fileHandle = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    setFileUploadError(null);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setFileUploaded(progress);
+      },
+      (err) => {
+        setFileUploadError(err);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
+          setFormData({ ...formData, avatar: downloadUrl })
+        );
+      }
+    );
+  };
+
   return (
     <section className="max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4 ">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/.*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           className="rounded-full w-14 h-14 object-cover self-center cursor-pointer"
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt=""
         />
+        <p className="text-sm text-center font-extrabold">
+          {" "}
+          {fileUploadError ? (
+            <span className="text-red-700">Failed Image Upload</span>
+          ) : fileUploaded > 1 && fileUploaded < 100 ? (
+            <span className="text-slate-700">{`Image uploading ${fileUploaded}%`}</span>
+          ) : fileUploaded === 100 && !fileUploadError ? (
+            <span className="text-green-700">Image Successfully Uploaded</span>
+          ) : (
+            ""
+          )}
+        </p>
+
         <input
           type="text"
           id="username"
